002dd7c062165f3630bc18bd2de02fc177aca3b4
Палаткина Регина <rnpl0103@mail.ru>
2023-03-03 12:46:13 +0500

    5-th commit

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,13 +1,11 @@
 from django.conf import settings
-
 from django.shortcuts import get_object_or_404, render
 
 from .models import Group, Post
 
-"""Главная страница."""
-
 
 def index(request):
+    """Главная страница."""
     posts = Post.objects.order_by('-pub_date')[:settings.POSTS_ON_PAGE]
     context = {
         'posts': posts,
@@ -15,10 +13,8 @@ def index(request):
     return render(request, 'posts/index.html', context)
 
 
-"""Страница с постами."""
-
-
 def group_posts(request, slug):
+    """Страница с постами."""
     group = get_object_or_404(Group, slug=slug)
     posts = group.posts.order_by('-pub_date')[:settings.POSTS_ON_PAGE]
     context = {
