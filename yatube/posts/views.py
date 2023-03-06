8907576c4315e5d6f41cfdf3560226a65f1a59ae
Палаткина Регина <rnpl0103@mail.ru>
2023-03-06 15:12:48 +0500

    2-nd commit

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator
 from django.conf import settings
 from django.shortcuts import get_object_or_404, render
 
@@ -7,8 +8,13 @@ from .models import Group, Post
 def index(request):
     """Главная страница."""
     posts = Post.objects.order_by('-pub_date')[:settings.POSTS_ON_PAGE]
+    post_list = Post.objects.all().order_by('-pub_date')
+    paginator = Paginator(post_list, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
     context = {
         'posts': posts,
+        'page_obj': page_obj,
     }
     return render(request, 'posts/index.html', context)
 
